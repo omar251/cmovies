bb8400c2b38b138524f2ffca62b6277b324690ee
C <mrc@gmail.com>
2025-07-13 12:57:20 +0300

    Add fzf-based movie selection interface
    Add pyfzf dependency and implement interactive movie selection using fzf
    prompt instead of simple console output

--- movie.py
+++ movie.py
@@ -1,16 +1,26 @@
 from imdb import IMDb
+from pyfzf.pyfzf import FzfPrompt
 
 # Create an instance of the IMDb class
 ia = IMDb()
-movie = input("Enter movie title: ")
+
 # Search for a movie
-movies = ia.search_movie(movie)
+query = input("Enter movie title: ")
+movies = ia.search_movie(query)
 
-# Print the search results
+# Prepare the data for fzf
+movie_list = []
 for movie in movies:
-    print(f"Title: {movie['title']}, Year: {movie.get('year')}, IMDb ID: {movie.movieID}")
+    movie_info = f"Title: {movie.get('title', 'N/A')}, Year: {movie.get('year', 'N/A')}, IMDb ID: {movie.movieID}"
+    movie_list.append(movie_info)
 
-# If you want to get the first result
-if movies:
-    first_movie = movies[0]
-    print(f"First result - Title: {first_movie['title']}, Year: {first_movie.get('year')}, IMDb ID: {first_movie.movieID}")
+# Use FzfPrompt to select a movie
+fzf = FzfPrompt()
+selected_movie_info = fzf.prompt(movie_list)
+
+if selected_movie_info:
+    selected_movie = selected_movie_info[0]
+    movie_id = selected_movie.split("IMDb ID: ")[1]
+    print(f"Selected Movie ID: {movie_id}")
+else:
+    print("No movie selected.")
